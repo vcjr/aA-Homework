dacbb4954273bd2d3a85bc92c827a6bb982c5205
Victor Crispin <victorrcrispinjr@gmail.com>
2020-06-10 10:08:53 -0400

    Homework

--- W4D3/lib/board.rb
+++ W4D3/lib/board.rb
@@ -2,36 +2,38 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @name1, @name2 = name1, name2
-
-    @cups = Array.new(14) { Array.new } 
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
     place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
-    @cups.each_with_index do |cup, i|
-      next if i == 6 || i == 13
-      4.times do 
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
         cup << :stone
       end
     end
   end
 
   def valid_move?(start_pos)
-   raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
-   raise "Starting cup is empty" if @cups[start_pos].empty?
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    # empties cup
     stones = @cups[start_pos]
     @cups[start_pos] = []
 
+    # distributes stones
     cup_idx = start_pos
     until stones.empty?
       cup_idx += 1
       cup_idx = 0 if cup_idx > 13
-      
+      # places stones in the correct current player's cups
       if cup_idx == 6
         @cups[6] << stones.pop if current_player_name == @name1
       elsif cup_idx == 13
@@ -46,14 +48,17 @@ class Board
   end
 
   def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
     if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
       :prompt
     elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
       :switch
     else
+      # ended on cup with stones in it -- automatically starts there
       ending_cup_idx
     end
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
   def render
@@ -65,8 +70,8 @@ class Board
   end
 
   def one_side_empty?
-    @cups.take(6).all? {|cup| cup.empty? } ||
-    @cups.drop(6).all? {|cup| cup.empty? }
+    @cups.take(6).all? { |cup| cup.empty? } ||
+    @cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
--- W4D3/lib/player.rb
+++ W4D3/lib/player.rb
@@ -8,7 +8,7 @@ class Player
 
   def prompt
     puts "#{@name}, please enter which cup you would like to start from: "
-    start_pos = gets.chomp.to_i
+    start_pos = gets.strip.to_i
     if (start_pos.between?(1,6) && @side != 1 ||
         start_pos.between?(7,12) && @side != 2)
       raise "Not your side!"
