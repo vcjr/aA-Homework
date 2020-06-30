fca68e58f8e547b527242b48fb6f0bdf20fde04e
Victor Crispin <victorrcrispinjr@gmail.com>
2020-06-30 19:12:01 -0400

    More added ES6 Fun
    ;
    .
    z
    !!!

--- W8D3/callback.js
+++ W8D3/callback.js
@@ -16,5 +16,30 @@ let timeToWait = 5000;
 // global.setTimeout(() => {
 //     console.log("It has been 5 seconds!")
 // }, timeToWait);
+// const _5SecondTimeOut = global.setTimeout(() => console.log("Even smaller arrow function!"), timeToWait);
+// global.setTimeout(() => console.log("Even smaller arrow function!"), timeToWait);
 
-global.setTimeout(()=> console.log("Even smaller arrow function!"), timeToWait);
+
+// Object
+
+cat = {
+    name: "Milo",
+    age: 9,
+    purr: () => console.log("meow!!!")
+};
+// Accessing as Key -> Value
+// console.log(cat["name"]);
+// console.log(cat["age"]);
+
+// // Accessing as a method name
+// console.log(cat.name);
+// console.log(cat.age);
+
+cat.purr();
+
+// console.log(cat.age + 1);
+// Reassigning properties
+cat.age += 1
+cat.name = "Chilo"
+console.log(cat.age);
+console.log(cat.name);
