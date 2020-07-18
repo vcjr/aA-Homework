093436a2ffe3cdc72e7cd5201acf74a46074dfe8
Victor Crispin <victorrcrispinjr@gmail.com>
2020-07-18 11:43:11 -0400

    Homeworks W10D4&D5

---
+++ W10D4/fruit_stand_01/frontend/actions.js
@@ -0,0 +1,15 @@
+export const addOrange = {
+  type: 'ADD_FRUIT',
+  fruit: 'orange'
+};
+
+// same action type different payload
+export const addApple = {
+  type: 'ADD_FRUIT',
+  fruit: 'apple'
+};
+
+export const addMango = {
+  type: 'ADD_FRUIT',
+  fruit: 'mango'
+};
---
+++ W10D5/middleware/frontend/todo_redux.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+function addLoggingToDispatch (store) {
+  let stored = store.dispatch;
+  return (action) => {
+    console.log(store.getState());
+
+    console.log(action);
+    store.dispatch(action);
+    console.log(store.getState());
+
+  };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+  
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
